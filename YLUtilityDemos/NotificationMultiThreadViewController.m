8d6218e8336e24b6e263cd81040bcc1921b71960
Mr_db <jxjgssylsg@126.com>
2016-10-16 17:11:42 +0800

    // 方法51:LocalNotification 本地通知

---
+++ YLUtilityDemos/NotificationMultiThreadViewController.m
@@ -0,0 +1,91 @@
+//
+//  ViewController.m
+//  Notification 与多线程以及 Notification 安全
+//
+//  Created by yifan on 15/9/15.
+//  Copyright (c) 2015年 黄成都. All rights reserved.
+//
+
+
+
+#import "NotificationMultiThreadViewController.h"
+
+static NSString *TEST_NOTIFICATION = @"TEST_NOTIFICATION";
+
+@interface NotificationMultiThreadViewController () <NSMachPortDelegate>
+@property (nonatomic) NSMutableArray    *notifications;         // 通知队列
+@property (nonatomic) NSThread          *notificationThread;    // 期望线程
+@property (nonatomic) NSLock            *notificationLock;      // 用于对通知队列加锁的锁对象，避免线程冲突
+@property (nonatomic) NSMachPort        *notificationPort;      // 用于向期望线程发送信号的通信端口
+
+@end
+
+@implementation NotificationMultiThreadViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"current thread = %@", [NSThread currentThread]);
+    
+    // 初始化
+    self.notifications = [[NSMutableArray alloc] init];
+    self.notificationLock = [[NSLock alloc] init];
+    
+    self.notificationThread = [NSThread currentThread];
+    self.notificationPort = [[NSMachPort alloc] init];
+    self.notificationPort.delegate = self;
+    
+    // 往当前线程的 run loop 添加端口源
+    // 当 Mach 消息到达而接收线程的 run loop 没有运行时，则内核会保存这条消息，直到下一次进入run loop
+    [[NSRunLoop currentRunLoop] addPort:self.notificationPort
+                                forMode:(__bridge NSString *)kCFRunLoopCommonModes];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(processNotification:) name:TEST_NOTIFICATION object:nil];
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        
+        [[NSNotificationCenter defaultCenter] postNotificationName:TEST_NOTIFICATION object:nil userInfo:nil];
+        
+    });
+}
+
+#pragma mark - NSMachPortDelegate 代理方法
+
+-(void)handleMachMessage:(void *)msg {
+    [self.notificationLock lock];
+    
+    while ([self.notifications count]) {
+        NSNotification *notification = [self.notifications objectAtIndex:0];
+        [self.notifications removeObjectAtIndex:0];
+        [self.notificationLock unlock];
+        [self processNotification:notification];
+        [self.notificationLock lock];
+    };
+    
+    [self.notificationLock unlock];
+}
+
+- (void)processNotification:(NSNotification *)notification {
+    if ([NSThread currentThread] != _notificationThread) {
+        // 如果当前线程不在要求的线程。转换到那个线程。
+        [self.notificationLock lock];
+        [self.notifications addObject:notification];
+        [self.notificationLock unlock];
+        //发送一条 Mach 消息、会调用代理方法
+        [self.notificationPort sendBeforeDate:[NSDate date]
+                                   components:nil
+                                         from:nil
+                                     reserved:0];
+    }
+    else {
+        // 处理通知
+        NSLog(@"current thread = %@", [NSThread currentThread]);
+        NSLog(@"process notification");
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
